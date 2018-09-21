5b51282d50efcfed62f3f90b2aa0d4b9b91fb552
AppXab <ab@appxtechnologies.com>
2018-09-21 19:20:58 +0530

    add GET/Book route

--- npa/server/server.js
+++ npa/server/server.js
@@ -12,21 +12,28 @@ var app=express();
 app.use(bodyParser.json());
 const port=process.env.PORT||3000;
 
+
+//post req //save to db
 app.post('/Book', (req, res) => { //books is collection name
   // console.log(req.body);
   var book = new Book({
     bookName: req.body.bookName
   })
-
   book.save().then((doc) => {
     res.send(doc);
   }, (e) => { 
     res.status(404).send(e);
-
   });
 });
 
-
+//get req //fetch data from db
+app.get('/Book',(req,res)=>{
+  Book.find().then((book)=>{
+    res.send({book});
+  },(e)=>{
+    res.status(400).send(e);
+  })
+})
 
 
 
